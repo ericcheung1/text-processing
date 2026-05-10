62cf8b5fea96c79c6c58aa82b5ad8536ae68f816
Eric Cheung <ericcheun251@gmail.com>
2026-05-09 22:38:24 -0700

    Add completed keywords core fastapi app

--- models/keywords/core.py
+++ models/keywords/core.py
@@ -1,8 +1,4 @@
-from common.schemas import payload, texts
-import yake
-
-text_idx, pred_result_idx = 0, 0
-id_idx = 1
+from common.schemas import payload
 
 def process_inputs(in_payload: payload) -> list[dict]:
     """
@@ -50,26 +46,3 @@ def extract_keywords(custom_yake, input_texts):
 
     return output
 
-if __name__ == "__main__":
-    from common.schemas import texts
-
-    text1 = texts(text="I love to eat nachos")
-    text2 = texts(text="I hate to eat nachos")
-    text3 = texts(text="This is just for dev ;)")
-
-    pl = payload(texts=[text1, text2, text3])
-
-    custom_kw_extractor = yake.KeywordExtractor(
-    lan="en",              # language
-    n=3,                   # ngram size
-    dedupLim=0.9,          # deduplication threshold
-    dedupFunc='seqm',      # deduplication function
-    windowsSize=1,         # context window
-    top=10,                # number of keywords to extract
-    features=None          # custom features
-    )
-
-    txs = process_inputs(pl)
-    raw_kws = extract_keywords(custom_kw_extractor, txs)
-
-    print(raw_kws)
--- models/keywords/main.py
+++ models/keywords/main.py
@@ -1,29 +1,32 @@
 import uvicorn
 from fastapi import FastAPI, status
 from common.schemas import payload
+from models.keywords.core import process_inputs, extract_keywords
 import yake
 
-# ;))
+app = FastAPI(docs_url=None, redoc_url=None)
 
-app = FastAPI()
-
-custom_kw_extractor = yake.KeywordExtractor(
+kw_extractor = yake.KeywordExtractor(
     lan="en",              # language
     n=3,                   # ngram size
     dedupLim=0.9,          # deduplication threshold
     dedupFunc='seqm',      # deduplication function
     windowsSize=1,         # context window
     top=10,                # number of keywords to extract
-    features=None          # custom features
+    features=None,         # custom features
 )
 
 @app.get("/health", status_code=status.HTTP_201_CREATED)
 def ping():
     return {"message": "service ok"}
 
-@app.post("/sentiment")
-def process_sentiment(input_payload: payload):
-    pass
+@app.post("/keywords")
+def keywords(input_payload: payload):  
+
+    processed_inputs = process_inputs(input_payload)
+    kws = extract_keywords(kw_extractor, processed_inputs)
+    
+    return kws
 
 if __name__ == "__main__":
-    uvicorn.run("models.sentiment.main:app", reload=True)
+    uvicorn.run("models.keywords.main:app", reload=True)
